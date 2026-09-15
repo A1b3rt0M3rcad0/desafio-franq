9896ae66a7c29165bf841ccccdd23de979053caa
Alberto S. Mercado <109180196+A1b3rt0M3rcad0@users.noreply.github.com>
2026-09-15 10:13:21 -0300

    chore: expose context manager components

--- package/agent/context/__init__.py
+++ package/agent/context/__init__.py
@@ -1,1 +1,12 @@
-"""Package marker."""
+from package.agent.context.builder import ContextBuilder
+from package.agent.context.manager import ContextManager
+from package.agent.context.models import AgentContext, ConversationTurn, SkillDescriptor, ToolDescriptor
+
+__all__ = [
+    "AgentContext",
+    "ContextBuilder",
+    "ContextManager",
+    "ConversationTurn",
+    "SkillDescriptor",
+    "ToolDescriptor",
+]
