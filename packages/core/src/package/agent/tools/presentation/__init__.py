450acb292cba87ff21a89fb68e35e808f7cb2ffe
Alberto S. Mercado <109180196+A1b3rt0M3rcad0@users.noreply.github.com>
2026-09-15 20:02:05 -0300

    feat: add presentation tool package

---
+++ packages/core/src/package/agent/tools/presentation/__init__.py
@@ -0,0 +1,4 @@
+from package.agent.tools.presentation.models import PresentationSpec, VisualizationSpec
+from package.agent.tools.presentation.tool import PresentationTool
+
+__all__ = ["PresentationSpec", "PresentationTool", "VisualizationSpec"]
